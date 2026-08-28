38a05168a2823d02d2718b0795abece9be8c3d70
kisor1968 <kisor1968@gmail.com>
2026-08-28 20:04:03 +0530

    Refactor Book_sell.py to improve structure and clarity

--- Book_sell.py
+++ Book_sell.py
@@ -63,10 +63,7 @@ with col_title:
     title_color1 = "#2ca02c"
     title_color2 = "#1f77b4"
     st.markdown(f"<h1 style='color: {title_color1};'>Textbook Exchange Platform</h1>", unsafe_allow_html=True)
-    st.markdown(
-    f"<p align='center' style='color: {title_color1}; font-size: 16px; font-style: italic;'>Maintained by</p>", 
-    unsafe_allow_html=True
-)
+    st.write(f"<div align='center' style='color: {title_color1}; font-style: italic; font-size: 16px;'>Maintained by</div>", unsafe_allow_html=True)
     st.markdown(f"<h1 style='color: {title_color2};'>Prabhu Jagatbandhu College</h1>", unsafe_allow_html=True)
     #st.title("Prabhu Jagatbandhu College")
     st.subheader("Campus Textbook Exchange Programme")

75d7f65466a75e5e421b781487d4d77591ddcc0b
kisor1968 <kisor1968@gmail.com>
2026-08-28 20:02:17 +0530

    Update Book_sell.py

--- Book_sell.py
+++ Book_sell.py
@@ -64,7 +64,7 @@ with col_title:
     title_color2 = "#1f77b4"
     st.markdown(f"<h1 style='color: {title_color1};'>Textbook Exchange Platform</h1>", unsafe_allow_html=True)
     st.markdown(
-    f"<center><span style='color: {title_color1}; font-size: 16px; font-style: italic;'>Maintained by</span></center>", 
+    f"<p align='center' style='color: {title_color1}; font-size: 16px; font-style: italic;'>Maintained by</p>", 
     unsafe_allow_html=True
 )
     st.markdown(f"<h1 style='color: {title_color2};'>Prabhu Jagatbandhu College</h1>", unsafe_allow_html=True)

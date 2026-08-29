b570cdde4c75385a9d62c7396c69541d510ed224
kisor1968 <kisor1968@gmail.com>
2026-08-29 15:35:03 +0530

    Update Book_sell.py

--- Book_sell.py
+++ Book_sell.py
@@ -120,7 +120,7 @@ with col_logo:
 
 with col_title:
     #title_color1 = "#2ca02c"
-     title_color1 = "#111111"
+    title_color1 = "#111111"
     title_color2 = "#1f77b4"
     title_color3 = "#1E88E5"
     st.markdown(f"<h1 style='color: {title_color1}; margin-bottom: 0px;'>Textbook Exchange Platform</h1>", unsafe_allow_html=True)
